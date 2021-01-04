90e133a6e5141092ea90f2d91152c7203ad7add0
Playwo <eliaswolf2001@t-online.de>
2021-01-04 15:56:39 +0100

    Fix js function names

--- WebAssemblyGameTemplate/Client/wwwroot/js/WebAssemblyHook.js
+++ WebAssemblyGameTemplate/Client/wwwroot/js/WebAssemblyHook.js
@@ -4,18 +4,18 @@ async function WebAssembly_CreatePlayerAsync() {
     await DotNet.invokeMethodAsync(assemblyName, 'CreatePlayerAsync');
 }
 
-async function WebAssembly_CreatePlayerAsync(playerInfo) {
+async function WebAssembly_UpdatePlayerAsync(playerInfo) {
     await DotNet.invokeMethodAsync(assemblyName, 'UpdatePlayerAsync', playerInfo);
 }
 
-async function WebAssembly_CreatePlayerAsync(loginCode) {
+async function WebAssembly_LoginStateAsync(loginCode) {
     await DotNet.invokeMethodAsync(assemblyName, 'LoginStateAsync', loginCode);
 }
 
-async function WebAssembly_CreatePlayerAsync(saveState) {
+async function WebAssembly_UpdateStateAsync(saveState) {
     await DotNet.invokeMethodAsync(assemblyName, 'UpdateStateAsync', saveState);
 }
 
-async function WebAssembly_CreatePlayerAsync() {
+async function WebAssembly_ActivateStateAsync() {
     await DotNet.invokeMethodAsync(assemblyName, 'ActivateTabAsync');
 }
